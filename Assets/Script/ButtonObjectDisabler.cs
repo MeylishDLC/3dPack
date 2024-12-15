c7eb5774250c845c0bf17dae5da01cb2c17d4209
MeylishDLC <moxie567@gmail.com>
2024-12-15 18:02:36 +0300

    wall disappears from box

--- Assets/Script/ButtonObjectDisabler.cs
+++ Assets/Script/ButtonObjectDisabler.cs
@@ -1,9 +1,36 @@
+using System;
 using UnityEngine;
 
 namespace Script
 {
     public class ButtonObjectDisabler : MonoBehaviour
     {
-    
+        [SerializeField] private GameObject objectToDisable;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private LayerMask playerLayer;
+        [SerializeField] private GameObject objectToPutOnButton;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (CheckIfCanDisable(other))
+            {
+                objectToDisable.SetActive(false);
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (CheckIfCanDisable(other))
+            {
+                objectToDisable.SetActive(true);
+            }
+        }
+
+        private bool CheckIfCanDisable(Collider other)
+        {
+            if (((1 << other.gameObject.layer) & playerLayer.value) != 0)
+            {
+                return true;
+            }
+            return other.gameObject == objectToPutOnButton;
+        }
     }
 }
